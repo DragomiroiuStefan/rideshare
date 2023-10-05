2259d8380abd898bb00ead4a27f63de1804ed46f
dragomiroiustefan <dragomiroiustefan@gmail.com>
2023-10-05 09:50:48 +0300

    Configured Logging

--- src/main/java/com/stefandragomiroiu/rideshare/controller/VehicleController.java
+++ src/main/java/com/stefandragomiroiu/rideshare/controller/VehicleController.java
@@ -6,6 +6,8 @@ import com.stefandragomiroiu.rideshare.controller.validators.RequestValidator;
 import com.stefandragomiroiu.rideshare.repository.UserRepository;
 import com.stefandragomiroiu.rideshare.repository.VehicleRepository;
 import com.stefandragomiroiu.rideshare.tables.pojos.Vehicle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +17,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/vehicles")
 class VehicleController {
+
+    private static final Logger logger = LoggerFactory.getLogger(VehicleController.class);
 
     public static final String USER_NOT_FOUND_ERROR_MESSAGE = "User %s not found";
     public static final String VEHICLE_NOT_FOUND_ERROR_MESSAGE = "Vehicle %s not found";
@@ -47,27 +51,37 @@ class VehicleController {
                 .orElseThrow(() -> new ResourceNotFoundException(errorMessage));
     }
 
+    /**
+     * {@code POST  /vehicle} : Create a new vehicle.
+     * @param vehicle to create
+     * @return {@code 201 (Created)} with created vehicle in response body or {@code 409 (conflict)} if vehicle id already exists or {@code 404 (Not Found)} if vehicle owner doesn't exist
+     */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Vehicle create(@RequestBody Vehicle vehicle) {
+        logger.info("Create vehicle request: {}", vehicle);
         vehicleValidator.validate(vehicle);
 
         if (vehicleRepository.findOptionalById(vehicle.getPlateNumber()).isPresent()) {
             String errorMessage = String.format(VEHICLE_ALREADY_EXISTS_ERROR_MESSAGE, vehicle.getPlateNumber());
             throw new ResourceAlreadyExistsException(errorMessage);
         }
+
         if (userRepository.findOptionalById(vehicle.getOwner()).isEmpty()) {
             String errorMessage = String.format(USER_NOT_FOUND_ERROR_MESSAGE, vehicle.getOwner());
             throw new ResourceNotFoundException(errorMessage);
         }
 
         vehicleRepository.insert(vehicle);
+
+
         return vehicle;
     }
 
     @PutMapping("/{plateNumber}")
     @ResponseStatus(HttpStatus.OK)
     public void update(@PathVariable String plateNumber, @RequestBody Vehicle vehicle) {
+        logger.info("Update vehicle request: {}", vehicle);
         if (vehicleRepository.findOptionalById(plateNumber).isEmpty()) {
             String errorMessage = String.format(VEHICLE_NOT_FOUND_ERROR_MESSAGE, plateNumber);
             throw new ResourceNotFoundException(errorMessage);
@@ -78,6 +92,7 @@ class VehicleController {
     @DeleteMapping("/{plateNumber}")
     @ResponseStatus(HttpStatus.OK)
     public void delete(@PathVariable String plateNumber) {
+        logger.info("Delete vehicle request: {}", plateNumber);
         if (vehicleRepository.findOptionalById(plateNumber).isEmpty()) {
             String errorMessage = String.format(VEHICLE_NOT_FOUND_ERROR_MESSAGE, plateNumber);
             throw new ResourceNotFoundException(errorMessage);
